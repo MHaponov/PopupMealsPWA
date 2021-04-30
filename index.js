9313bd430370a4b6384d3b47cf1ed86f2a30dec6
Maksym Haponov <gaponovm08@gmail.com>
2021-04-30 14:39:49 -0400

    Fix payment

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ module.exports = class extends Page {
             oEntity.id = key;
             sResult += `<div style="text-align: center;"> 
             <h2>${oEntity.title}</h2>
-            <p><img style="min-width: 300px; max-width: 400px;" src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
+            <p><img style="min-width: 300px; max-width: 400px;" src="${oEntity.featured_image}" alt="${oEntity.title}" /></p>
             <p>${oEntity.full_description}</p>
             <form action="https://popupmeals.herokuapp.com/payment/" method="post">
             <input type="hidden" name="title" value="${oEntity.title}" />
-            <input type="hidden" name="price" value="${oEntity.price}" />
+            <input type="hidden" name="price" value="${oEntity.price * 0.13 + oEntity.price}" />
             <input type="tel" placeholder="enter your number" name="telephone"/>
             <button type="submit">Order now</button>
             </form>
